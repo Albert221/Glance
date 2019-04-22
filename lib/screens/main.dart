2a433b4012cc3add564f5e9f264b3b022be45d9c
Albert221 <w.albert221@gmail.com>
2019-04-22 15:55:11 +0200

    Add infinite scroll

--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -4,9 +4,39 @@ import 'package:reddigram/store/store.dart';
 import 'package:reddigram/widgets/widgets.dart';
 import 'package:redux/redux.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final _scrollController = ScrollController();
+
+  double Function() _offsetToLoad = () => 0;
+  VoidCallback _fetchMore = () {};
+
+  @override
+  void initState() {
+    super.initState();
+
+    _scrollController.addListener(() {
+      final position = _scrollController.position;
+      if (position.pixels + _offsetToLoad() >= position.maxScrollExtent) {
+        _fetchMore();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    _offsetToLoad = () => MediaQuery.of(context).size.height * 3;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -24,22 +54,27 @@ class MainScreen extends StatelessWidget {
       body: StoreConnector<ReddigramState, _ViewModel>(
         onInit: (store) => store.dispatch(fetchMoreFeed()),
         converter: (store) => _ViewModel.fromStore(store),
-        builder: (context, vm) => ListView.builder(
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              itemCount: vm.feedState.photos.length + 1,
-              itemBuilder: (context, i) {
-                // Is last?
-                if (i == vm.feedState.photos.length) {
-                  return Container(
-                    padding: const EdgeInsets.symmetric(vertical: 32.0),
-                    alignment: Alignment.center,
-                    child: const CircularProgressIndicator(),
-                  );
-                }
+        builder: (context, vm) {
+          _fetchMore = vm.fetchMore;
 
-                return PhotoListItem(photo: vm.feedState.photos[i]);
-              },
-            ),
+          return ListView.builder(
+            controller: _scrollController,
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            itemCount: vm.feedState.photos.length + 1,
+            itemBuilder: (context, i) {
+              // Is last?
+              if (i == vm.feedState.photos.length) {
+                return Container(
+                  padding: const EdgeInsets.symmetric(vertical: 32.0),
+                  alignment: Alignment.center,
+                  child: const CircularProgressIndicator(),
+                );
+              }
+
+              return PhotoListItem(photo: vm.feedState.photos[i]);
+            },
+          );
+        },
       ),
     );
   }
@@ -56,7 +91,11 @@ class _ViewModel {
   factory _ViewModel.fromStore(Store<ReddigramState> store) {
     return _ViewModel(
       feedState: store.state.feedState,
-      fetchMore: () => store.dispatch(fetchMoreFeed()),
+      fetchMore: () {
+        if (!store.state.feedState.fetching) {
+          store.dispatch(fetchMoreFeed());
+        }
+      },
     );
   }
 }
