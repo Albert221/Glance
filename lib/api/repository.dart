4a326f21507d804743127c2a6161d626491767a9
Albert221 <w.albert221@gmail.com>
2019-04-22 16:24:32 +0200

    Send correct User-Agent to Reddit API

--- lib/api/repository.dart
+++ lib/api/repository.dart
@@ -2,9 +2,23 @@ import 'dart:convert';
 
 import 'package:built_collection/built_collection.dart';
 import 'package:http/http.dart' as http;
+import 'package:package_info/package_info.dart';
 import 'package:reddigram/api/api.dart';
 
 class RedditRepository {
+  static PackageInfo _packageInfo;
+
+  static Future<http.Response> _get(String url) async {
+    if (_packageInfo == null) {
+      _packageInfo = await PackageInfo.fromPlatform();
+    }
+
+    return http.get(url, headers: {
+      'User-Agent':
+          '${_packageInfo.packageName}:${_packageInfo.version} (by /u/Albert221)'
+    });
+  }
+
   static ListingResponse _filterOnlyPhotos(ListingResponse response) {
     return response.rebuild((b) => b
       ..data = response.data
@@ -14,11 +28,9 @@ class RedditRepository {
           .toBuilder());
   }
 
-  // FIXME: Change limit back to 25
   static Future<ListingResponse> subreddit(String name,
-      {String after = '', int limit = 3}) async {
-    return http
-        .get('https://reddit.com/r/$name.json?after=$after&limit=$limit')
+      {String after = '', int limit = 25}) async {
+    return _get('https://reddit.com/r/$name.json?after=$after&limit=$limit')
         .then((response) => serializers.deserializeWith(
             ListingResponse.serializer, json.decode(response.body)))
         .then(_filterOnlyPhotos);
