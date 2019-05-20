a0dea8a0b11cff7410916ea9f310d12c31ef59b1
Albert221 <w.albert221@gmail.com>
2019-05-20 20:46:17 +0200

    Load preferences in one action

--- lib/store/preferences/actions.dart
+++ lib/store/preferences/actions.dart
@@ -1,3 +1,4 @@
+import 'package:meta/meta.dart';
 import 'package:reddigram/store/store.dart';
 import 'package:redux/redux.dart';
 import 'package:redux_thunk/redux_thunk.dart';
@@ -7,18 +8,18 @@ ThunkAction<ReddigramState> loadPreferences() {
   return (Store<ReddigramState> store) async {
     final prefs = await SharedPreferences.getInstance();
 
-    store.dispatch(SetTheme(
-      prefs.getString("theme") == "dark" ? AppTheme.dark : AppTheme.light,
+    store.dispatch(SetPreferencesBulk(
+      theme:
+          prefs.getString('theme') == 'dark' ? AppTheme.dark : AppTheme.light,
+      showNsfw: prefs.getBool('show_nsfw') ?? false,
     ));
-
-    store.dispatch(SetShowNsfw(prefs.getBool("show_nsfw") ?? false));
   };
 }
 
 ThunkAction<ReddigramState> setTheme(AppTheme theme) {
   return (Store<ReddigramState> store) async {
     (await SharedPreferences.getInstance())
-        .setString("theme", theme.toString());
+        .setString('theme', theme.toString());
 
     store.dispatch(SetTheme(theme));
   };
@@ -26,10 +27,19 @@ ThunkAction<ReddigramState> setTheme(AppTheme theme) {
 
 ThunkAction<ReddigramState> setShowNsfw(bool showNsfw) {
   return (Store<ReddigramState> store) async {
-    (await SharedPreferences.getInstance()).setBool("show_nsfw", showNsfw);
+    (await SharedPreferences.getInstance()).setBool('show_nsfw', showNsfw);
 
     store.dispatch(SetShowNsfw(showNsfw));
   };
+}
+
+class SetPreferencesBulk {
+  final AppTheme theme;
+  final bool showNsfw;
+
+  SetPreferencesBulk({@required this.theme, @required this.showNsfw})
+      : assert(theme != null),
+        assert(showNsfw != null);
 }
 
 class SetTheme {
--- lib/store/preferences/reducer.dart
+++ lib/store/preferences/reducer.dart
@@ -2,9 +2,17 @@ import 'package:reddigram/store/store.dart';
 import 'package:redux/redux.dart';
 
 Reducer<PreferencesState> preferencesReducer = combineReducers([
+  TypedReducer<PreferencesState, SetPreferencesBulk>(_setPreferencesBulk),
   TypedReducer<PreferencesState, SetTheme>(_setTheme),
   TypedReducer<PreferencesState, SetShowNsfw>(_setShowNsfw),
 ]);
+
+PreferencesState _setPreferencesBulk(
+    PreferencesState state, SetPreferencesBulk action) {
+  return state.rebuild((b) => b
+    ..theme = action.theme
+    ..showNsfw = action.showNsfw);
+}
 
 PreferencesState _setTheme(PreferencesState state, SetTheme action) {
   return state.rebuild((b) => b..theme = action.theme);
