baf1417bfd96d50a22c6e386392a0ae58f99f081
Albert221 <w.albert221@gmail.com>
2019-06-10 01:51:00 +0200

    Remove commented exports from api/api.dart

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -4,11 +4,6 @@ export 'api_repository/api.dart';
 export 'api_repository/local.dart';
 export 'api_repository/facade.dart';
 
-//export 'response_models/link_listing_response.dart';
-//export 'response_models/subreddit_response.dart';
-//export 'response_models/subreddit_info_response.dart';
-//export 'response_models/serializers.dart';
-
 export 'mappers/link_listing_photos_mapper.dart';
 export 'mappers/subreddit_info_mapper.dart';
 
