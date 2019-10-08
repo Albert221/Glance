c1abd51de043cebfca9ff3151b0ae79c6cb135ae
Albert221 <w.albert221@gmail.com>
2019-10-08 13:20:29 +0200

    Flattened screens/main directory

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,7 +1,7 @@
-export 'main/main.dart';
-export 'main/preferences_sheet.dart';
-export 'main/feed_tab.dart';
-export 'main/subbed_tab.dart';
+export 'main.dart';
+export 'preferences_sheet.dart';
+export 'feed_tab.dart';
+export 'subbed_tab.dart';
 export 'photo_preview.dart';
 export 'subreddit.dart';
 export 'preferences.dart';
